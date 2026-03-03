947c0c9dcb7caba3fa5f9b24ac655b9925823500
suryateja kandukuru <suryateja_kandukuru@epam.com>
2026-03-03 13:33:37 +0530

    feat: integrated react-router and setup dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, NavLink, Link } from "react-router";
+import Dashboard from './features/dashboard/dashboard';
+
+function AppWrapper() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <nav>
+      {/* NavLink makes it easy to show active states */}
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      >
+        Home
+      </NavLink>
+
+      <Link to="/dashboard">Dashboard</Link>
+    </nav>
   )
 }
 
-export default App
+export default AppWrapper
