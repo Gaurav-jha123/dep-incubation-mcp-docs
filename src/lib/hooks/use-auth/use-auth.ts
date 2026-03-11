c61b6bb69c067d6ffcc6d6e04347671e0b457591
suryateja kandukuru <suryateja_kandukuru@epam.com>
2026-03-11 19:22:23 +0530

    fix: build login

--- src/lib/hooks/use-auth/use-auth.ts
+++ src/lib/hooks/use-auth/use-auth.ts
@@ -22,7 +22,7 @@ function useAuth() {
       };
       
     const login = (data: z.infer<typeof loginFormSchema>) => {
-        console.info('user details:', data.emailId)
+        console.info('user details:', data.email)
         // assuming sending this email id , password to backend;
         // const { emailId, password } = data;
         const token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmTmFtZSI6IlN1cnlhdGVqYSIsImxOYW1lIjoiS2FuZHVrdXJ1IiwiZW1haWxJZCI6InN1cnlhdGVqYV9rYW5kdWt1cnVAZXBhbS5jb20iLCJpYXQiOjE3NzI1MzUyNDUsImV4cCI6MTgwNjY5ODc1N30.BfPhvH1lUl3tkRW5ur78hhJo2gzJ-DNJnu8h_BqnbRE`;
